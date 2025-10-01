afd55bd9ee66e4e158d1620719429a82049ce63b
Ash <ashvalebooks@gmail.com>
2025-10-01 09:59:55 -0600

    Updates

--- launchtimer.js
+++ launchtimer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var launchCountDownDate = new Date("December 31, 2025 21:00:00").getTime();
+var launchCountDownDate = new Date("December 31, 2025 22:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
